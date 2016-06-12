c38fac3af32d8d4dad51f719a7560653e61cb73a
Mireia <mireia@mireiasangalo.com>
2016-06-12 15:39:38 +0100

    add maps tutorial

--- GoogleMaps-workshop/3-AddingMaps/main.js
+++ GoogleMaps-workshop/3-AddingMaps/main.js
@@ -1,12 +1,10 @@
-//Create and configure the map
+//This is our function called initMap
 function initMap() {
-
-  // A reference to the div that the map will be loaded in (getElementById)
-  var mapDiv = document.getElementById('map');
-  // Here we create a new Goggle Maps object
-  var map = new google.maps.Map(mapDiv , {
-    // Options for the map: center and zoom
-    center: {lat: 44.540, lng: -78.546},
-    zoom: 8
-  });
+    // Here we reference to the <div> that the map will be loaded into by using getElementById
+    var mapDiv = document.getElementById('map');
+   //Now we set our custom options for ur map, which are 'center' -> tells the API where to center the map and 'zoom' -> sets the zoom level
+   var map = new google.maps.Map(mapDiv, {
+       center: {lat: 44.540, lng: -78.546},
+       zoom: 8
+   });
 }
